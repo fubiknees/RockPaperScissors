8717fd76ad24b75a1d71f35909b8a35268d44263
fubiknees <85858509+fubiknees@users.noreply.github.com>
2023-12-28 21:52:45 -0800

    Buttons working! Dom output methods used and working correctly.

--- rps.js
+++ rps.js
@@ -2,83 +2,95 @@ let playerScore = 0;
 let computerScore= 0; 
 let tieScore= 0; 
 
-const rockButton = document.querySelector('#rock');
-const paperButton = document.querySelector('#paper');
-const scissorsButton = document.querySelector('#scissors');
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
 const resultDiv = document.querySelector('.result')
  
 
 
-function game ( ) { 
+// function game ( ) { 
 
 function getComputerChoice ( ) {    
-
   const options = ['ROCK', 'PAPER', 'SCISSORS'] ; 
-
   return options[Math.floor(Math.random( )* options.length)];   
-
   }    
 
-   
-
 function playRound (playerSelection,  computerSelection) {   
-
+    console.log('1', playerSelection, '2', computerSelection)
      if ( playerSelection === computerSelection) {   
 
             const p = document.createElement('p')
             p.innerText = "TIE GAME! AI predicted your choice of " + playerSelection + " ." + "Play Again!"
             tieScore+= 1; 
+            resultDiv.appendChild(p);
+            
 
-         } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {    
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {    
 
-             alert("You lose! Paper beats Rock. ");   
-
+            const p = document.createElement('p')
+            p.innerText = "You lose! Paper beats Rock."
              computerScore+= 1; 
+             resultDiv.appendChild(p);
 
-         } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {   
-
-            alert("You Win! Rock beats Scissors. Bing Bang Boom");   
-
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {   
+            
+            const p = document.createElement('p')
+            p.innerText = "You Win! Rock beats Scissors. Bing Bang Boom";   
             playerScore+= 1; 
+            resultDiv.appendChild(p);
 
-        } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {    
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {    
 
-             alert("You Win! Paper beats Rock. Night Night Geodude");   
-
+            const p = document.createElement('p')
+            p.innerText = "You Win! Paper beats Rock. Night Night Geodude";   
              playerScore+= 1; 
+             resultDiv.appendChild(p);
 
-         } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {   
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {   
+            const p = document.createElement('p')
+            p.innerText = "You Lose! Scissors beats Paper.";   
+            computerScore+= 1; 
+            resultDiv.appendChild(p);
 
-            alert ("You Lose! Scissors beats Paper.");   
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {   
 
-             computerScore+= 1; 
+            const p = document.createElement('p')
+            p.innerText = "You Lose! Rock beats Scissors. Better luck next time";  
+            computerScore+= 1; 
+            resultDiv.appendChild(p);
 
-        } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {   
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {   
 
-             alert("You Lose! Rock beats Scissors. Better luck next time");  
-
-             computerScore+= 1; 
-
-       } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {   
-
-             alert ("You Win! Scissors beats Paper.");  
-
-                playerScore+= 1; 
+            const p = document.createElement('p')
+            p.innerText = "You Win! Scissors beats Paper.";  
+            playerScore+= 1; 
+            resultDiv.appendChild(p);
 
       }   
+ }   
 
-}   
-
-rockButton.addEventListener('click, () => {
-    const playerSelection = 'rock'
+rockButton.addEventListener('click', () => {
     const computerSelection = getComputerChoice ( );  
+    const playerSelection = 'ROCK'
     playRound(playerSelection, computerSelection);
-}
+})
+
+paperButton.addEventListener('click', () => {
+    const playerSelection = 'PAPER'
+    const computerSelection = getComputerChoice ( ); 
+    playRound(playerSelection, computerSelection);
+})
+
+scissorsButton.addEventListener('click', () => {
+    const playerSelection = 'SCISSORS'
+    const computerSelection = getComputerChoice ( ); 
+    playRound(playerSelection, computerSelection);
+})
 
 
  
 
-game ( ); 
 
 // game ( ); 
 
